626365b4bf5246d7e5639942995111ef9c6ffde0
Bartosz <73258197+bartekwawrzkowicz@users.noreply.github.com>
2021-09-19 11:58:58 +0200

    adding logic for the main function

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-// BUTTONS --------------------------------------------------
-const hiHat = document.getElementById('hh');
-const snare = document.getElementById('sd');
-const bassDrum = document.getElementById('bd');
-const leftCymbal = document.getElementById('lc');
-const leftTom = document.getElementById('lt');
-const rightTom = document.getElementById('rt');
-const bassTom = document.getElementById('bt');
-const rightCymbal = document.getElementById('rc');
-
 // PICTURES -------------------------------------------------
 const picClean = document.getElementById('clean');
 const picHiHat = document.getElementById('hi-hat');
@@ -19,32 +9,32 @@ const picRightTom = document.getElementById('right-tom');
 const picBassTom = document.getElementById('bass-tom');
 const picRightCymbal = document.getElementById('right-cymbal');
 
+// LOGIC ----------------------------------------------------
 
-toggleView = (picture) => {
-    picture.classList.toggle('non-active');
-    picClean.classList.toggle('non-active');
-    setTimeout(() => {
-        picture.classList.toggle('non-active'), picClean.classList.toggle('non-active')
-    }, 100);
-}
+const playDrum = event => {
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    const key = document.querySelector(`button[data-sound="${event.keyCode}"]`);
+    const image = document.querySelector(`img[data-key="${event.keyCode}"]`);
 
-toggleButtonView = (button) => {
-    button.classList.toggle('active');
-    setTimeout(() => {
-        button.classList.toggle('active')
-    }, 100);
-}
-
-playDrum = event => {
-    const audio = document.querySelector(`audio[data-key="${event.keyCode}"`);
     if (!audio) return;
     audio.currentTime = 0;
+    audio.play();
 
-    audio.play()
+    key.classList.add('active');
 
+    picClean.classList.add('non-active');
+    image.classList.remove('non-active');
 }
 
+const removeTransition = event => {
+    const key = document.querySelector(`button[data-sound="${event.keyCode}"`);
+    const image = document.querySelector(`img[data-key="${event.keyCode}"]`);
 
+    key.classList.remove('active');
 
+    picClean.classList.remove('non-active');
+    image.classList.add('non-active');
+}
 
 window.addEventListener('keydown', playDrum);
+window.addEventListener('keyup', removeTransition);
